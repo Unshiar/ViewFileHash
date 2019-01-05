a5679752d3383be13d05b67136086e01dac1b8c7
Ryabinchuk Sergey <sergosr.ryab@yandex.ru>
2019-01-05 16:52:30 +0300

    MessageHelper changes
    messageHelp small changes

--- MessageHelper.h
+++ MessageHelper.h
@@ -16,7 +16,7 @@ const char messageForFile[] = "For file:\n";
 const char messageTypeHelp[] = "Type vfhash --help for help\n";
 
 const char messageHelp[] = "Usage: vfhash <filename> <algoritms> \n"
-                           "filename\t- file what you want see hash\n"
+                           "filename\t- the file whose hash you want to view\n"
                            "algoritms\t- hash algoritms ( md5 md4 sha1 sha224 sha256 sha384 sha512 sha3_224 sha3_256 sha3_384 sha3_512 keccak_224 keccak_256 keccak_384 keccak_512 )\n\n"
                            "For example:\n"
                            "vfhash file1.txt md4 md5\n"
